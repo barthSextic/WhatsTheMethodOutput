6abf85269200d1094a2ed7b948d5d0423d69a53d
barthSextic <the.rickhowell@gmail.com>
2022-10-08 11:45:45 -0400

    Created Main Method

--- src/WhatsTheMethodOutput.java
+++ src/WhatsTheMethodOutput.java
@@ -9,6 +9,8 @@ public class WhatsTheMethodOutput {
         int nextYear;
         int thisMonth;
 
+
+
         nextYear = ++thisYear + CENTURY % 333;
         thisMonth = leap(nextYear,28);
         System.out.println(thisYear + " " + nextYear + " " + thisMonth);
